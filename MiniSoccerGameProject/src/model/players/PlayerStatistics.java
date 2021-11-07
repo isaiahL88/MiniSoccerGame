8cf3f39e6c369e7cdfc76b9cecf764aa76830a00
isaiahL88 <isaiah.ib@outlook.com>
2021-11-06 22:06:09 -0400

    Finished Player statistics class
    - DONE BY CHUN-KIT CHUNG

---
+++ MiniSoccerGameProject/src/model/players/PlayerStatistics.java
@@ -0,0 +1,19 @@
+package model.players;
+
+public class PlayerStatistics {
+
+    private Integer stats; 
+
+    PlayerStatistics(){
+        stats = 0;
+    }
+
+    public Integer getStatistics() {
+        return stats;
+    }
+
+    public void setStatistics(Integer stats) {
+        this.stats = stats;
+    }
+
+}
